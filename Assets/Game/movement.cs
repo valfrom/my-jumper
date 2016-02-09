c686f8ee6fa8c5b9ab7fa0bc4fd3f221c182470c
Stepan Peknyy <stepan_peknyy@ukr.net>
2016-02-09 18:18:35 +0200

    Є меню з кнопкою «Старт», та ігровий режим, але він далекий від завершення. Ігровий процес і меню не зв’язані.

---
+++ Assets/Game/movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	public float speed = 50f;
+	public float jumpPower =150f;
+
+	public bool grounded;
+
+	private Rigidbody2D rb2d;
+	void Start()
+	{
+		rb2d = gameObject.GetComponent<Rigidbody2D > ();
+	}
+	void Update()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		rb2d.AddForce ((Vector2.right * speed) * h);
+
+		if (Input.GetButtonDown ("Jump")  ) {
+			rb2d.AddForce (Vector2.up * jumpPower*10);  
+
+			}
+		}
+}
---
+++ Assets/Menu/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public void PlayGame ()
+	{
+		Application.LoadLevel (1);
+	}
+	public void ExitGame ()
+	{
+		Application.Quit ();
+	}
+	
+
+}

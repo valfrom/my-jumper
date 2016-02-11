dbe07fe16a8a9e72c5d51b9866fd1b4196970d50
Stepan Peknyy <stepan_peknyy@ukr.net>
2016-02-11 20:34:40 +0200

    3
    Тепер монетка зв'являється кожних 0,3с , а по X в проміжку(-5,5),
    відстань між ними по Y завжди однакова. Зірки 1 за секунду і відстань по
    Y теж змінюється. Для цього є спеціальний скріпт Run, коментпрів до
    нього поки що немає.

--- Assets/Game/CoinsSpawn.cs
+++ Assets/Game/CoinsSpawn.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class CoinsSpawn : MonoBehaviour {
 
 	public GameObject coin;
-	public GameObject jumper;
-	private Vector2 velocity;
 
-	public float smoothTimeY;
+	private Vector2 velocity;
+    private GameObject cam;
+    public float smoothTimeY;
 	public float smoothTimeX;
 
 	void Start() {
-		jumper = GameObject.FindGameObjectWithTag("Jumper")  ;
+        cam = GameObject.FindGameObjectWithTag("Run");
 		StartCoroutine (Inst ());
 	}
 
@@ -22,11 +22,12 @@ public class CoinsSpawn : MonoBehaviour {
 	}
 		
 	void FixedUpdate(){
-		float posX = Mathf.SmoothDamp (transform.position.x, jumper.transform.position.x, ref velocity.x, smoothTimeX);
-		float posY = Mathf.SmoothDamp (transform.position.y,jumper.transform.position.y, ref velocity.y, smoothTimeY );
+        
+        float posX = Mathf.SmoothDamp(transform.position.x, cam.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, cam.transform.position.y, ref velocity.y, smoothTimeY);
 
-		transform.position = new Vector3 (posX+ Random.Range(-2,2), posY + 10 , transform.position.z);
-	}
+        transform.position = new Vector3(posX + Random.Range(-5, 5), posY+20 , transform.position.z);
+    }
 
 	void Repeat() {
 		StartCoroutine (Inst ());
